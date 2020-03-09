02083e742369844dc718e430dd6f38516f080f57
Veneche <venecheleigh@gmail.com>
2020-03-09 10:11:26 +1000

    Completed module

--- src/main/java/com/pluralsight/ControllerServlet.java
+++ src/main/java/com/pluralsight/ControllerServlet.java
@@ -65,6 +65,10 @@ public class ControllerServlet extends HttpServlet {
 				case "/edit":
 					showEditForm(request, response);
 		  break;
+				case "/update":
+					updateBook(request, response);
+		  break;
+		  
         default:
 				   listBooks(request, response);
            break;
@@ -129,6 +133,20 @@ public class ControllerServlet extends HttpServlet {
 
 		bookDAO.insertBook(newBook);
 		response.sendRedirect("list");
+	}
+	
+	private void updateBook(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException { 
+		int id = Integer.parseInt(request.getParameter("id"));
+		String title = request.getParameter("booktitle");
+		String author = request.getParameter("bookauthor");
+		String price = request.getParameter("bookprice");
+		
+		Book updateBook = new Book(id, title, author, Float.parseFloat(price));
+		
+		bookDAO.updateBook(updateBook);
+		response.sendRedirect("list");
+		
 	}
 
 	/**
